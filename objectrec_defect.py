8186eba8da011e334e655965f4f5fa4671bd40a4
tushar nitave <tushar nitave>
2017-07-23 21:12:07 +0530

    Modifications in fun callback

--- objectrec_defect.py
+++ objectrec_defect.py
@@ -4,6 +4,8 @@ Version 1.0 without GUI.
 This project uses OpenCV libraries for identifying different objects trained and identifying minor defects.
 We have used only Open Source tools for development. Spyder(Python 3.6) as our primary IDE and Fedora 24.
 The project was completed within one month as part of our internship.
+Here 'Hero' and 'Boxer' are name of the objects we were supposed to identify. 
+Please train and add your own object and change the name accordingly.
 
 """
 
@@ -33,33 +35,35 @@ def callback():
     boxer_count=0
     hero_count=0
     
-  #  ti=datetime.datetime.now()
-  #  string_i_want=('%02d'%(ti.second))[:-4]
-  #  print(string_i_want)
+	# Imports system time for terminating the program automatically after 5 seconds. 
   
     termination_time_system1 = datetime.now()
     termination_time_start = termination_time_system1.second % 5
     print ("start",termination_time_start)
     
+	#synchronising the time with system time. 
     time.sleep(1)
   
+	
     while True:
         
-        
+        # Imports system time and compares with termination_time_system1
+		
         termination_time_system2 = datetime.now()
         termination_time_check = termination_time_system2.second % 5
         print ("current",termination_time_check)      
         select = 0
         
         ret, img = cap.read()
+		
         if ret is True:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             boxer = boxer_cascade.detectMultiScale(gray,1.3,5)
             hero = hero_cascade.detectMultiScale(gray,1.3,5)
        
-        
+			# For loop is used of identifying object. You can add as many loops depending on number of objects.
+			
             for (x,y,w,h) in boxer:
-                #cls()
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 #cv2.putText(img,'BOXER',(x-w, y-h), font,0.5, (0,255,255), 1,cv2.LINE_AA)
                 #cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,0), 2)
@@ -67,8 +71,6 @@ def callback():
                 roi_color = img[y:y+h, x:x+w]
                 print ("boxer_",boxer_count)
                 boxer_count=boxer_count+1
-                #select=2
-                #time.sleep(0.5)
         
   
             for (x,y,w,h) in hero:
@@ -79,64 +81,42 @@ def callback():
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = img[y:y+h, x:x+w]
                 print ("hero_        ",hero_count)
-                hero_count=hero_count+1
-                #select=1
-                #time.sleep(0.5)
-
-            
-            
-
-        
-            
+                hero_count=hero_count+1            
         
             cv2.imshow('Detecting', img)
             k = cv2.waitKey(30) & 0xff
             if k == 27 or termination_time_start == termination_time_check:
-                
               break
       
     
     ret,img=cap.read()
-   # cv2.imshow('capture.jpg',img)
+    #cv2.imshow('capture.jpg',img)
+	
+	#stores the final output result in home directory.
     cv2.imwrite('C:\\Users\\tushar\\Desktop\\Mutha\\capture.jpg',img)
        
-    
     cap.release()
     cv2.destroyAllWindows()
     
-    
-    
-    
-    
-    
-    
-    
+    #depending on which object is found select variable is assigned.
     
     if(boxer_count>hero_count):
-    
-     print("boxer found")
+     print("Boxer found")
      select=2
+	 
     elif (boxer_count<hero_count):
-     print("hero found")
+     print("Hero found")
      select=1
+	 
     else:
-     print("no object detected")
+     print("No object detected")
      if(select==0):
       app.infoBox("title", "NO object found")
      
-     
-     
-     
-     
-     
-     
-     
-     
-     
+  
 
     img_rgb = cv2.imread('capture.jpg')
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    ###########################################
     #template of defects to be found are to be added here
     template_array2 = ["temp1_2.jpg","ipadboxer.jpg"]
     
@@ -188,9 +168,7 @@ def callback():
         app.infoBox("title", "fault detected in hero")
     if(select==2 and counter1 != 5):
         app.infoBox("title", "fault detected in boxer")
-   
-        
-        
+ 
 
 callback()
 app.go()
